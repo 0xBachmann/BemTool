34e8b1339cec9c5b9f91ae68d936b6270935bfcd
Jonas Bachmann <jonas.bachmann@inf.ethz.ch>
2026-05-05 17:05:04 +0200

    fix spelling

--- build_and_test.sh
+++ build_and_test.sh
@@ -5,8 +5,9 @@ cmake -DCMAKE_BUILD_TYPE=Release ..
 make -j16
 
 cd tests || exit
-echo "Testing Derivatives of rotating Green's functino"
+echo "Testing Derivatives of rotating Green's function"
 ./test_kernel_normal_derivative
 
 echo "Testing stationary operators and layer potential matching Helmholtz formulations from BemTool"
+echo "May take up to 30 minutes"
 ./test_zero_omega_matches_helmholtz
